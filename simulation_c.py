a458f02af64211f0cc80ee7b72d5f8987b07cebc
shachar731945 <shachar731945@gmail.com>
2018-03-12 07:49:47 +0200

    Add files via upload

---
+++ simulation_c.py
@@ -0,0 +1,18 @@
+from input_processor import InputProcessor
+from client_network_manager import ClientNetworkManager
+
+
+def main():
+    client = ClientNetworkManager("192.168.0.20", 8845)
+    input_processor = InputProcessor(client)
+    input_processor.initialize()
+    from time import sleep
+    sleep(10)
+    input_processor.stop()
+    client.close_connection()
+
+
+if __name__ == "__main__":
+    main()
+
+
