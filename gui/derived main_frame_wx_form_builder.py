ad3e3b79aad2a6db41ae25ec421b3f28edcad925
shachar731945 <shachar731945@gmail.com>
2018-06-03 19:49:38 +0300

    update commit

--- gui/derived main_frame_wx_form_builder.py
+++ gui/derived main_frame_wx_form_builder.py
@@ -48,19 +48,11 @@ class ProgramFrame(MainFrame):
         self.__connecting_new_clients_thread = None
         self._server_pc_coordinates = None
 
-        # self.computers_list_ctrl_matrix.InsertColumn(0, "computer number",
-        #                                              width=150)
-        # self.computers_list_ctrl_matrix.InsertColumn(1, "coordinates",
-        #                                              width=100)
-
         self.computers_list_ctrl.InsertColumn(
             0, "pc number", width=150)
         self.computers_list_ctrl.InsertColumn(
             1, "address", width=150)
         self._address_number_dictionary = {}
-        # self.computers_list_ctrl.InsertColumn(
-        #     2, "coordinates", wx.LIST_FORMAT_LEFT,
-        #     width=100)
 
         self.__new_connection_evt_id = wx.NewId()
         self.__result_event = ResultEvent("", self.__new_connection_evt_id)
@@ -68,22 +60,6 @@ class ProgramFrame(MainFrame):
         self.confirm_state = Event()
 
         self._index = 1
-
-    # def update_matrix(self, event):
-    #     x_ratio = int(self.x_text_ctrl.GetValue())
-    #     y_ratio = int(self.y_text_ctrl.GetValue())
-    #     computer_number = int(self.pc_num_list_ctrl.GetValue())
-    #     for pc_number in self._address_number_dictionary.keys():
-    #         if pc_number == computer_number:
-    #             self.pc_matrix.set(
-    #                 x_ratio, y_ratio,
-    #                 Computer(self._address_number_dictionary[pc_number]))
-    #             self.computers_list_ctrl_matrix.SetItem(
-    #                 pc_number - 1, 1,
-    #                 UPDATE_MATRIX_COORDINATES_MESSAGE.format(x_ratio, y_ratio))
-    #             if pc_number == 2:
-    #                 self.pc_matrix.set_pointer((x_ratio, y_ratio))
-    #     print(x_ratio, y_ratio, computer_number)
 
     def new_connection_update(self, event):
         self.__lock.acquire()
@@ -98,15 +74,8 @@ class ProgramFrame(MainFrame):
         self.__lock.release()
 
     def start_new_session_panel(self, event):
-        # self.status_bar.SetStatusText("computer number 1", 0)
-        # self.join_wxisting_session_panel.Disable()
-        # self.join_wxisting_session_panel.Hide()
         self.computers_list_ctrl.DeleteAllItems()
         self.__server_netwrok_manager = ServerNetworkManager()
-        # temp_byted_pc_address = str(
-        #     (get_local_ip_address(),ab
-        #      self.__server_netwrok_manager.port)).encode("utf-8")
-        # self.message_list_ctrl.AppendText(b64encode(temp_byted_pc_address))
         addr = ip_address(get_local_ip_address())
         self.message_list_ctrl.AppendText(encode(int(addr)))
         self.__connecting_new_clients_thread = Thread(
@@ -117,14 +86,16 @@ class ProgramFrame(MainFrame):
         evt_connect(
             self, self.new_connection_update, self.__new_connection_evt_id)
         self.__connecting_new_clients_thread.start()
+        self.join_wxisting_session_panel.Disable()
+        self.join_wxisting_session_panel.Hide()
         self.new_session_panel.Enable()
         self.new_session_panel.Show()
         self.Update()
         self.Layout()
 
     def join_existing_session_panel(self, event):
-        # self.new_session_panel.Disable()
-        # self.new_session_panel.Hide()
+        self.new_session_panel.Disable()
+        self.new_session_panel.Hide()
         self.join_wxisting_session_panel.Enable()
         self.join_wxisting_session_panel.Show()
         self.Update()
@@ -144,25 +115,6 @@ class ProgramFrame(MainFrame):
 
     def start_session(self, event):
         self._session_client.start_session()
-
-    # this is the old form_session version
-    # def form_session(self, event):
-    #     print("am i here?")
-    #     self.confirm_state.set()
-    #     evt_dissconnect(self, self.new_connection_update,
-    #                     self.__new_connection_evt_id)
-    #     pc_num = len(self._address_number_dictionary.keys())
-    #     self.pc_matrix = Matrix(pc_num, pc_num)
-    #     print(self._address_number_dictionary)
-    #     for pc_number in self._address_number_dictionary.keys():
-    #         print(pc_number)
-    #         self.computers_list_ctrl_matrix.InsertItem(maxsize, str(pc_number))
-    #     self.new_session_panel.Disable()
-    #     self.new_session_panel.Hide()
-    #     self.form_matrix_panel.Enable()
-    #     self.form_matrix_panel.Show()
-    #     self.Update()
-    #     self.Layout()
 
     def form_session(self, event):
         print("am i here?")
